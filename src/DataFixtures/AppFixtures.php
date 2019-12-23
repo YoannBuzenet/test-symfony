ad40b5fa6d186c01deeb29eb97f3a52598195d02
Yoann Buzenet <ybuzenet@gmail.com>
2019-12-23 22:00:02 +0100

    adding pictures to DB, adding show method to Ads

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -12,9 +13,12 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
-        $title = $faker->sentence();
+        
         
         for( $i=1; $i<=30; $i++){
+
+            $title = $faker->sentence();
+
             $ad = new Ad();
             $ad->setTitle($title)
             ->setCoverImage($faker->imageUrl(1000,350))
@@ -22,6 +26,16 @@ class AppFixtures extends Fixture
             ->setContent('<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>')
             ->setPrice(mt_rand(40,200))
             ->setRooms(mt_rand(1,5));
+
+            for($j=1 ; $j <= mt_rand(2,5) ; $j++){
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                      ->setCaption($faker->sentence())
+                      ->setAd($ad);
+
+                $manager->persist($image);      
+            }
 
             $manager->persist($ad);
         }
