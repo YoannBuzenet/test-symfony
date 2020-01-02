cee7816e2fc5d0db3abecbdff900569ec1c8f60c
Yoann <ybuzenet@gmail.com>
2020-01-02 11:31:19 +0100

    seeting up User login with Symfony security component

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccountController extends AbstractController
+{
+    /**
+     * Display and handle login form
+     * 
+     * @Route("/login", name="account_login")
+     * 
+     * @return Response
+     * 
+     */
+    public function login()
+    {
+        return $this->render('account/login.html.twig');
+    }
+
+    /**
+     * Allows logout
+     * 
+     * @route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout(){
+        // Nada
+    }
+}
