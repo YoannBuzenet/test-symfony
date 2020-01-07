6c728f0045a71dee34dc02b8df04bab2441b0653
Yoann <ybuzenet@gmail.com>
2020-01-07 21:17:45 +0100

    created the Admin role

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -22,6 +23,24 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
+
+        //Handling  Roles
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        //special admin user
+        $adminUser = new User();
+        $adminUser->setFirstName('Yoann')
+                  ->setLastName('Buzenet')
+                  ->setEmail('yoann.buzenet@gmail.com')
+                  ->setHash($this->encoder->encodePassword($adminUser,'password'))
+                  ->setPicture('https://avatars.io/twitter/yoann_buzenet')
+                  ->setIntroduction($faker->sentence())
+                  ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3)) . '</p>')
+                  ->addUserRole($adminRole);
+        
+        $manager->persist($adminUser);
         
         //Here we handle Users
 
