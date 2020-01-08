d730e1452c17a3402234167c4b539a7011ba58ca
Yoann <ybuzenet@gmail.com>
2020-01-08 11:48:15 +0100

    adding user verification and template enhancements

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use App\Entity\PasswordUpdate;
 use App\Entity\User;
 use App\Form\AccountType;
-use App\Form\PasswordUpdateType;
+use App\Entity\PasswordUpdate;
 use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -88,6 +89,7 @@ class AccountController extends AbstractController
      * Displays and handle editing profile form
      *
      * @Route("/account/profile", name="account_profile")
+     * @IsGranted("ROLE_USER")
      * 
      * @return Response
      */
@@ -119,6 +121,7 @@ class AccountController extends AbstractController
      * Edit password
      * 
      * @Route("/account/password-update", name="account_password")
+     * @IsGranted("ROLE_USER")
      *
      * @return Response
      */
@@ -167,6 +170,7 @@ class AccountController extends AbstractController
      * @return Response
      * 
      * @Route("/account", name="account_index")
+     * @IsGranted("ROLE_USER")
      * 
      */
     public function myAccount(){
