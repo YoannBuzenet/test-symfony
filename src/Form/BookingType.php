ef6cb8a78927b637d60e723844b75993850127a9
Yoann <ybuzenet@gmail.com>
2020-01-10 13:39:50 +0100

    +booking controller + wiring booking form

---
+++ src/Form/BookingType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class BookingType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startDate', DateType::class, $this->getConfiguration('Date d\'arrivée', "Date à laquelle vous comptez arriver"))
+            ->add('endDate', DateType::class, $this->getConfiguration('Date de départ', "Date à laquelle vous quittez les lieux"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
