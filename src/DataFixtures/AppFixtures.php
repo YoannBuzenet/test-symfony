93f45d9a3249219991016db3d8558f91fd2e32b1
Yoann <ybuzenet@gmail.com>
2020-01-10 12:55:33 +0100

    + Booking Entity + update Fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use Faker\Factory;
 use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
+use App\Entity\Booking;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -42,7 +43,7 @@ class AppFixtures extends Fixture
         
         $manager->persist($adminUser);
         
-        //Here we handle Users
+        //Here we handle regular Users
 
         $users = [];
         $genders = ['male','female'];
@@ -102,6 +103,29 @@ class AppFixtures extends Fixture
                       ->setAd($ad);
 
                 $manager->persist($image);      
+            }
+
+            //Hadling Booking generation
+            for($j = 1; $j <= mt_rand(0,10); $j++){
+                $booking = new Booking();
+
+                $createdAt = $faker->dateTimeBetween('-6 months');
+                $startDate = $faker->dateTimeBetween('-3 months');
+                
+                $duration = mt_rand(3,10);
+                $endDate = (clone $startDate)->modify("+ $duration days");
+
+                $amount = $ad->getPrice() * $duration;
+
+                $booker = $users[mt_rand(0, count($users) -1)];
+                $booking->setBooker($booker)
+                        ->setAd($ad)
+                        ->setStartDate($startDate)
+                        ->setEndDate($endDate)
+                        ->setCreatedAt($createdAt)
+                        ->setAmount($amount);
+                
+                        $manager->persist($booking);
             }
 
             $manager->persist($ad);
